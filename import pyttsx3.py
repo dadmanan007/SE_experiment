756a611f795678ac9b32254acaaffa5e16e23e5b
dadmanan007 <145047393+dadmanan007@users.noreply.github.com>
2023-09-14 22:35:00 +0530

    Add files via upload

---
+++ import pyttsx3.py
@@ -0,0 +1,44 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[7].id)
+engine.setProperty('rate',170)
+
+def Speak(Audio):
+    print("   ")
+    print(f": {Audio}")
+    engine.say(Audio)
+    print("    ")
+    engine.runAndWait()
+
+def takecommand(): 
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("          ")
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"Your Command :  {query}\n")
+
+    except:   
+        return "None"
+        
+    return query.lower()
+
+def TaskExe():
+
+    while True:
+
+        query = takecommand()
+
+        if 'hello' in query:
+            Speak("Hello Sir , How Are You ?")
+
+        else:
+            Speak("No Command Found!")
